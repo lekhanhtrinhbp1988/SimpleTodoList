e8d40aaec053a95ff72a84736e853c54a27eac09
TRINHLE-LAPTOP\Le Khanh Trinh <lekhanhtrinhbp@gmail.com>
2021-10-31 16:15:10 +0700

    seed initial data by migration

--- SimpleTodoList/Entities/TodoDbContext.cs
+++ SimpleTodoList/Entities/TodoDbContext.cs
@@ -12,5 +12,13 @@ namespace SimpleTodoList.Entities
         {
         }
         public DbSet<Todo> Todos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Todo>().HasData(
+            new Todo { Id = 1, Title = "Become a rock star developer", Description = "Do it now" },
+            new Todo { Id = 2, Title = "Take fucking action", Description = "Do it now" });
+        }
     }
 }
--- SimpleTodoList/Migrations/20211031090353_SeedInitialData.Designer.cs
+++ SimpleTodoList/Migrations/20211031090353_SeedInitialData.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SimpleTodoList.Entities;
 
 namespace SimpleTodoList.Migrations
 {
     [DbContext(typeof(TodoDbContext))]
-    partial class TodoDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211031090353_SeedInitialData")]
+    partial class SeedInitialData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
