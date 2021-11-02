ae2da23183017d0ca5ee15ab7b825565f7ec4ad0
TRINHLE-LAPTOP\Le Khanh Trinh <lekhanhtrinhbp@gmail.com>
2021-11-02 09:37:46 +0700

    implement seed data when application start

---
+++ SimpleTodoList/Entities/DbInitializer.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleTodoList.Entities
+{
+    public static class DbInitializer
+    {
+        public static IHost InitData(this IHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var serviceProvider = scope.ServiceProvider;
+
+                InitializeAsync(serviceProvider).GetAwaiter().GetResult();
+            }
+            return host;
+        }
+
+        public static async Task InitializeAsync(IServiceProvider serviceProvider)
+        {
+            var context = serviceProvider.GetRequiredService<TodoDbContext>();
+
+            context.Database.EnsureCreated();
+
+            // Seed Todo
+            if (!context.Todos.Any())
+            {
+                var todos = new List<Todo>
+                {
+                    new Todo
+                    {
+                        Title = "Become a rock star developer",
+                        Description = "Do it now"
+                    },
+                   new Todo
+                   {
+                       Title = "Take fucking action",
+                       Description = "Do it now"
+                   }
+                };
+                context.Todos.AddRange(todos);
+                await context.SaveChangesAsync();
+            };
+        }
+    }
+}
